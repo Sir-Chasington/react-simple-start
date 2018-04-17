4b4ac725e55a2ea201b38128e0dcc499c24fccd5
Chase Lester <chaselester@Chases-MacBook-Pro.local>
2018-04-17 13:15:53 -0600

    react quick start initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Parcel simple react build
+
+# Install
+`yarn install`
+
+# Dev
+`yarn start`
+
+# Build
+`yarn build`
---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import '../less/main.less';
+
+const App = () => {
+  return <h3>Hello World!</h3>
+}
+
+export default App
